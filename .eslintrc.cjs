5e743af434f39e48a48007d8db895bfe086b5c23
Billèle Sahouli <b.sahouli@hotmail.fr>
2026-03-23 14:58:02 +0100

    chore: setup Tailwind, ESLint, Prettier, Stylelint, Pinia, VueUse, Vuelidate, i18n, nuxt-icon

---
+++ .eslintrc.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  root: true,
+  extends: [
+    '@nuxtjs/eslint-config-typescript',
+    'plugin:vue/vue3-recommended',
+    'prettier',
+  ],
+  rules: {},
+}
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@pinia/nuxt',
+    '@vueuse/nuxt',
+    '@nuxtjs/i18n',
+    '@nuxt/icon',
+  ],
+
+  i18n: {
+    defaultLocale: 'fr',
+    locales: [
+      { code: 'fr', language: 'fr-FR', file: 'fr.json', name: 'Français' },
+      { code: 'en', language: 'en-US', file: 'en.json', name: 'English' },
+    ],
+  },
 })
